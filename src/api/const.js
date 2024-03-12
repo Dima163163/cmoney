fccd1619c5da36f93eaaa9888f3420ce562a1114
Dima163163 <khlopovdima@gmail.com>
2024-03-12 17:31:32 +0400

    cmoney

--- src/api/const.js
+++ src/api/const.js
@@ -1,2 +1,2 @@
-export const API_URL = 'http://localhost:3000';
-export const URL_API_WS = 'ws://localhost:3000/currency-feed';
+export const API_URL = 'https://quaint-axiomatic-truffle.glitch.me';
+export const URL_API_WS = 'wss://quaint-axiomatic-truffle.glitch.me/currency-feed';
